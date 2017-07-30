73d14688f1b4664154e1212733734080aa12371b
Godfrey <godfreyjr@gmail.com>
2017-07-30 17:51:13 -0500

    Initial commit, set up files and and structure of calc

---
+++ calc.js
@@ -0,0 +1,18 @@
+
+var result = 0;
+
+function multiplication (num) {
+    return result *= num;
+}
+
+function division (num) {
+    return result /= num;
+}
+
+function addition (num) {
+    return result += num;
+}
+
+function subtraction (num) {
+    return result -= num;
+}
